26346783f747f1d8ab49061519eeccc18e1972b3
Alvaro <dralvphd2@gmail.com>
2022-10-26 20:27:09 -0300

    Trabalhando com condicionais

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class OlaMundo
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 7 - Condicionais");
+
+        int idadeJoao = 15;
+        int qtdPessoas = 2;
+
+        bool acompanhado = qtdPessoas > 0;
+       
+        if (idadeJoao >= 18 || acompanhado)
+        {
+            Console.WriteLine("Pode entrar");
+        }
+        else 
+        {
+            Console.WriteLine("Não pode entrar");
+        }
+    }
+}
