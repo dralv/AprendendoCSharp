8783bfead970e621a5ed76a416ab729c96c30f52
Alvaro <dralvphd2@gmail.com>
2022-10-26 15:39:21 -0300

    Trabalhando com Char

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 5");
+
+        char letra = 'a'; //armazenamento de um caracter
+        // o compilador entende o char como um número 
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+
+        letra = (char) (65 + 1); // transformando um inteiro em Char
+        Console.WriteLine(letra);
+
+        letra = (char)(66 + 1); 
+        Console.WriteLine(letra);
+    }
+}
