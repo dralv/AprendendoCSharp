ebf82df8ec76b8640f4f68ffc771ffa6fd28edaf
Alvaro <dralvphd2@gmail.com>
2022-10-26 08:45:22 -0300

    Trabalhando com Casting e outros tipos numéricos

---
+++ 4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class OlaMundo
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 4 - Convertendo Tipos e outros tipos numéricos");
+        double salario;
+        salario = 3000.15;
+
+        int salarioInteiro;
+        salarioInteiro =(int) salario;
+
+        long x;
+        x = 200000000000000;
+
+        short y;
+        y = 1500;
+
+        float altura = 1.62f;
+        
+        Console.WriteLine(altura);
+
+
+        Console.WriteLine(salarioInteiro);
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+
+        
+    }
+}
