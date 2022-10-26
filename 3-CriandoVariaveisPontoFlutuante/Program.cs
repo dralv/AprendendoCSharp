bbacf98002a3ab25bd0354b723296330d09e260a
Alvaro <dralvphd2@gmail.com>
2022-10-26 08:32:46 -0300

    Trabalhando com double

---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class OlaMundo
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Fluante");
+        double salario;
+        salario = 3000.10;
+
+        Console.WriteLine(salario);
+
+        salario = 3000;
+
+        Console.WriteLine(salario);
+    }
+}
