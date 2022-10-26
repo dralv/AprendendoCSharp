4895e70356ae41c5aafdf86a161405b8774a183e
Alvaro <dralvphd2@gmail.com>
2022-10-26 07:52:26 -0300

    Adicionar arquivos de projeto.

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class OlaMundo
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
---
+++ OlaMundo/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class OlaMundo
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá Mundo");
+    }
+}
