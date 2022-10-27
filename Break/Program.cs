5886f740006590a1033452acb7f709dc6ab64a2f
Alvaro <dralvphd2@gmail.com>
2022-10-27 08:44:41 -0300

    Conhecendo o comando break

---
+++ Break/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class OlaMundo
+{
+    static void Main(string[] args)
+    {
+        string desenho = "*";
+        for(int contadorLinhas  = 0; contadorLinhas < 10; contadorLinhas++)
+        {
+            for(int contadorColunas = 0;contadorColunas<10;contadorColunas++)
+            {
+
+                Console.Write("*");
+                if (contadorColunas >= contadorLinhas)
+                    break;
+            }
+            Console.WriteLine();
+        }
+    }
+}
