deb1cb9d806e3811332b175b05695e96910e9661
Alvaro <dralvphd2@gmail.com>
2022-10-27 08:23:05 -0300

    Trabalhando com  While e com for

---
+++ 10-CalculaPoupanca/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 10");
+
+        double investimento = 1000;
+        int mes = 1;
+        /*
+        while(mes <= 12){
+            investimento  = investimento + investimento* 0.005;
+            Console.WriteLine(investimento);
+            mes++;
+        }
+        */
+        
+        for (int i = 1; i <= 12; i++)
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine(investimento);
+        }
+        
+
+    }
+}
